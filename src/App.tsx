58a51a0cf666479127957b1eefd74ecbe369d376
Woshikuzzaman Anik <woshikuzzaman@gmail.com>
2024-07-01 13:03:03 +0600

    # feat: - create a standalone button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Button } from './components/ui/button'
+
 function App (): JSX.Element {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return <Button>Hello world!</Button>
 }
 
 export default App
