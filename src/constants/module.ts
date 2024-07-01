e73e2b257a9b2c0be98405857cf49e9156bd198a
Woshikuzzaman Anik <woshikuzzaman@gmail.com>
2024-07-01 13:50:04 +0600

    # refactor: - module data structure change from object to array

--- src/constants/module.ts
+++ src/constants/module.ts
@@ -1,48 +1,48 @@
 interface IIterationData {
-  [key: number]: {
-    title: string
-    variant: string
-    mode?: boolean
-  }
+  id: number
+  title: string
+  variant: string
+  mode?: boolean
 }
 
 interface IExperimentModuleData {
-  [key: number]: {
-    title: string
-    lock: boolean
-    iterations: IIterationData
-  }
+  id: number
+  title: string
+  lock: boolean
+  iterations: IIterationData[]
 }
 
-const moduleData: IExperimentModuleData = {
-  1: {
+const moduleData: IExperimentModuleData[] = [
+  {
+    id: 1,
     title: 'Experiment Module',
     lock: false,
-    iterations: {
-      1: {
+    iterations: [
+      {
+        id: 1,
         title: 'Iteration Title',
         variant: ''
       }
-    }
+    ]
   },
-  2: {
+  {
+    id: 2,
     title: 'Experiment Module',
     lock: true,
-    iterations: {
-      1: {
+    iterations: [
+      {
+        id: 1,
         title: 'Iteration Title',
         variant: ''
       },
-      2: {
+      {
+        id: 2,
         title: 'Iteration Title',
         variant: ''
       }
-    }
+    ]
   }
-}
+]
 
 export default moduleData
-export type {
-  IIterationData,
-  IExperimentModuleData
-}
+export type { IIterationData, IExperimentModuleData }
