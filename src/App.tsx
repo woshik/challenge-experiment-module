b137763e2549149561ae49dbf83de032bac43c26
Woshikuzzaman Anik <woshikuzzaman@gmail.com>
2024-07-01 13:08:37 +0600

    # feat: - create a standalone textarea component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Button } from './components/ui/button'
+import Textarea from './components/ui/textarea'
 
 function App (): JSX.Element {
-  return <Button>Hello world!</Button>
+  return (<><Button>Hello world!</Button><Textarea className='border mt-10' size='lg' /></>)
 }
 
 export default App
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,16 +2,20 @@ import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '../../lib/utils'
 
-interface IButtonProps extends
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
+interface IButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
   VariantProps<typeof buttonVariants> {}
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap text-primary-foreground rounded-lg text-md px-4 bg-transparent outline-none font-bold transition-colors shadow-sm disabled:pointer-events-none disabled:opacity-50',
+  `inline-flex items-center justify-center whitespace-nowrap 
+    text-primary-foreground rounded-lg text-md px-4 bg-transparent 
+    outline-none font-bold transition-colors shadow-sm disabled:pointer-events-none 
+    disabled:opacity-50`,
   {
     variants: {
       variant: {
-        default: 'border border-primary-foreground hover:border-white hover:text-white',
+        default:
+          'border border-primary-foreground hover:border-white hover:text-white',
         ghost: 'border-none hover:text-white',
         selected: 'border border-selection text-selection'
       },
@@ -27,7 +31,12 @@ const buttonVariants = cva(
   }
 )
 
-const Button = ({ className, variant, size, ...props }: IButtonProps): JSX.Element => {
+const Button = ({
+  className,
+  variant,
+  size,
+  ...props
+}: IButtonProps): JSX.Element => {
   return (
     <button
       className={cn(buttonVariants({ variant, size, className }))}
