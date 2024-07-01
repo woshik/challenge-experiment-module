4dabf6acce6b3809fcee2531703bbdf14a0743c0
Woshikuzzaman Anik <woshikuzzaman@gmail.com>
2024-07-01 13:57:06 +0600

    # feat: - create experiment module context and pass the module data

---
+++ src/context/experimentModule.tsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState } from 'react'
+import type { IExperimentModuleData } from '../constants/module'
+import moduleData from '../constants/module'
+
+interface IContext {
+  experimentModules: IExperimentModuleData[]
+  setExperimentModules: React.Dispatch<React.SetStateAction<IExperimentModuleData[]>>
+}
+
+interface ExperimentModuleProviderProps {
+  children: React.ReactNode
+}
+
+const ExperimentModuleContext = createContext<IContext | null>(null)
+
+const ExperimentModuleProvider = ({
+  children
+}: ExperimentModuleProviderProps): JSX.Element => {
+  const [experimentModules, setExperimentModules] = useState(moduleData)
+
+  return (
+    <ExperimentModuleContext.Provider
+      value={{
+        experimentModules,
+        setExperimentModules
+      }}
+    >
+      {children}
+    </ExperimentModuleContext.Provider>
+  )
+}
+
+const useExperimentModule = (): IContext => {
+  const { experimentModules, setExperimentModules } = useContext(
+    ExperimentModuleContext
+  ) as IContext
+
+  return {
+    experimentModules,
+    setExperimentModules
+  }
+}
+
+export { ExperimentModuleProvider, useExperimentModule }
