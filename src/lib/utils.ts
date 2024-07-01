1ef659df0d43b122a6d95c947f175576245df9a4
woshik <woshik@users.noreply.github.com>
2024-07-01 21:53:10 +0000

    deploy: aeed3f49d6b3a0a36a22325789ab0077e69994c5

--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-function cn (...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs))
-}
-
-export { cn }
